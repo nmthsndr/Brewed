b3fa61b82395644abb0437623b6361644c684956
nmthsndr <nmthsndr4@icloud.com>
2025-10-10 22:03:40 +0200

    commit

--- Brewed/Program.cs
+++ Brewed/Program.cs
@@ -1,34 +1,48 @@
 
+using Microsoft.OpenApi.Models;
+
 namespace Brewed
 {
+    using Microsoft.OpenApi.Models;
     public class Program
     {
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            // Swagger setup
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Brewer API",
+                    Version = "v1"
+                });
+            });
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Brewer API v1");
+                });
             }
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-
             app.MapControllers();
 
             app.Run();
+
         }
     }
 }
